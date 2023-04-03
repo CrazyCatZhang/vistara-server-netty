946262ad1874bcc0bcf2dd737f8e9d19306dea7b
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-04-03 20:36:00 +0800

    Create user entity class and Mapper class

---
+++ service/src/main/java/com/catzhang/im/service/user/dao/mapper/UserDataMapper.java
@@ -0,0 +1,9 @@
+package com.catzhang.im.service.user.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.catzhang.im.service.user.dao.UserDataEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserDataMapper extends BaseMapper<UserDataEntity> {
+}
