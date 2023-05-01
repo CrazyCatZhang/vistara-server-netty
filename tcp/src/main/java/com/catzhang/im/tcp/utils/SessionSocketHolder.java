7874d9e1baa265ff1127b88d35fad234c20e963f
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-05-01 20:07:42 +0800

    TCP gateway user offline and logout status notification

--- tcp/src/main/java/com/catzhang/im/tcp/utils/SessionSocketHolder.java
+++ tcp/src/main/java/com/catzhang/im/tcp/utils/SessionSocketHolder.java
@@ -1,10 +1,15 @@
 package com.catzhang.im.tcp.utils;
 
 import com.alibaba.fastjson.JSON;
+import com.catzhang.im.codec.pack.user.UserStatusChangeNotifyPack;
+import com.catzhang.im.codec.proto.MessageHeader;
 import com.catzhang.im.common.constant.Constants;
+import com.catzhang.im.common.enums.ConnectStatus;
 import com.catzhang.im.common.enums.ImConnectStatus;
+import com.catzhang.im.common.enums.command.UserEventCommand;
 import com.catzhang.im.common.model.UserClientDto;
 import com.catzhang.im.common.model.UserSession;
+import com.catzhang.im.tcp.publish.MessageProducer;
 import com.catzhang.im.tcp.redis.RedisManager;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.AttributeKey;
@@ -69,6 +74,18 @@ public class SessionSocketHolder {
         RedissonClient redissonClient = RedisManager.getRedissonClient();
         RMap<String, String> map = redissonClient.getMap(appId + Constants.RedisConstants.USER_SESSION_CONSTANTS + userId);
         map.remove(clientType + ":" + imei);
+
+        MessageHeader messageHeader = new MessageHeader();
+        messageHeader.setAppId(appId);
+        messageHeader.setImei(imei);
+        messageHeader.setClientType(clientType);
+
+        UserStatusChangeNotifyPack userStatusChangeNotifyPack = new UserStatusChangeNotifyPack();
+        userStatusChangeNotifyPack.setAppId(appId);
+        userStatusChangeNotifyPack.setUserId(userId);
+        userStatusChangeNotifyPack.setStatus(ConnectStatus.OFFLINE_STATUS.getCode());
+        MessageProducer.sendMessage(userStatusChangeNotifyPack, messageHeader, UserEventCommand.USER_ONLINE_STATUS_CHANGE.getCommand());
+
         nioSocketChannel.close();
     }
 
@@ -89,6 +106,17 @@ public class SessionSocketHolder {
             userSession.setConnectState(ImConnectStatus.OFFLINE_STATUS.getCode());
             map.put(clientType + ":" + imei, JSON.toJSONString(userSession));
         }
+
+        MessageHeader messageHeader = new MessageHeader();
+        messageHeader.setAppId(appId);
+        messageHeader.setImei(imei);
+        messageHeader.setClientType(clientType);
+
+        UserStatusChangeNotifyPack userStatusChangeNotifyPack = new UserStatusChangeNotifyPack();
+        userStatusChangeNotifyPack.setAppId(appId);
+        userStatusChangeNotifyPack.setUserId(userId);
+        userStatusChangeNotifyPack.setStatus(ConnectStatus.OFFLINE_STATUS.getCode());
+        MessageProducer.sendMessage(userStatusChangeNotifyPack, messageHeader, UserEventCommand.USER_ONLINE_STATUS_CHANGE.getCommand());
 
         nioSocketChannel.close();
     }
